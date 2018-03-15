f409570c33d8b12f38f37b9a7432dd4111a5a20e
Vaclav Malinak <vaclavm@kentico.com>
2018-03-15 13:38:58 +0100

    KA-276 Add new tests from services

---
+++ ToDoList/test/ToDoList.Services.Tests/Services/DateTimeServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ToDoList.Services.Tests.Services
+{
+    [TestFixture]
+    public class DateTimeServiceTests
+    {
+        [Test]
+        public void GetCurrentDateTime_ReturnsDateTimeNow()
+        {
+            // Arrange
+            var dateTimeService = new DateTimeService();
+            var expectedDateTime = DateTime.Now;
+
+            // Act
+            var actualDateTime = dateTimeService.GetCurrentDateTime();
+
+            // Assert
+            Assert.That(actualDateTime, Is.InRange(expectedDateTime, expectedDateTime.AddSeconds(5)), $"{actualDateTime} should be equal or greater (max by 5 seconds) to {expectedDateTime}");
+        }
+    }
+}
---
+++ ToDoList/test/ToDoList.Services.Tests/Services/IdentifierServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ToDoList.Services.Tests.Services
+{
+    [TestFixture]
+    public class IdentifierServiceTests
+    {
+        [Test]
+        public void GenerateIdentifier_NotEmptyGuid()
+        {
+            // Arrange
+            var identifierService = new IdentifierService();
+
+            // Act
+            var actualIdentifier = identifierService.GenerateIdentifier();
+
+            // Assert
+            Assert.That(actualIdentifier, Is.Not.EqualTo(Guid.Empty), $"{actualIdentifier} should not be empty");
+        }
+    }
+}
