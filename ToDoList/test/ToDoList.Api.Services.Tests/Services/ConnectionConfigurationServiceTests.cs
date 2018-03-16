dca2e7aeaaa9aea6bae6de01d4a6b2db72c7d409
Vaclav Malinak <vaclavm@kentico.com>
2018-03-16 15:11:58 +0100

    KA-276 Add tests from ConnectionConfigurationService

---
+++ ToDoList/test/ToDoList.Api.Services.Tests/Services/ConnectionConfigurationServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+using ToDoList.Api.Services.Services;
+
+namespace ToDoList.Api.Services.Tests.Services
+{
+    [TestFixture]
+    public class ConnectionConfigurationServiceTests
+    {
+        [Test]
+        public void ConnectionString()
+        {
+            // Act
+            var service = new ConnectionConfigurationService();
+            
+            // Assert
+            Assert.That(service.ConnectionString, Is.EqualTo("http://test.url/todolist"), "Connection string is not correctly retrieved");
+        }
+    }
+}
